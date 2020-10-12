72cc14e28befb49e452b3186c52452fc92ae9d8e
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-13 05:45:52 +0700

    Add files via upload

---
+++ tugas8.php
@@ -0,0 +1,32 @@
+<?php
+
+function ketupat(){
+  for( $a=13 ; $a>0 ; $a--){
+      for($b=1; $b<=$a; $b++){
+          echo "_";
+      }for($a1=13; $a1>$a; $a1--){
+         echo"x";
+      }for($a2=13; $a2>$a; $a2--){
+          echo"x";
+    }echo"<br>";
+  }for($k=0 ; $k<=13; $k++){
+     for($j=1 ; $j<=$k ; $j++){
+         echo"_";
+      }for($k1=13 ; $k1>$k ; $k1--){
+          echo"x" ;
+      }for($k2=13; $k2>$k; $k2--){
+            echo"x";
+      }echo"<br>";
+}
+}
+echo "<table><tr>";
+  for ($i = 0 ; $i<=4; $i++){
+    echo "<td>",ketupat(),"</td>";
+  }
+  echo "</tr>";
+  echo "<tr>";
+  for ($i = 0 ; $i<=4; $i++){
+    echo "<td>",ketupat(),"</td>";
+  }
+  echo "</tr></table>";
+?>
